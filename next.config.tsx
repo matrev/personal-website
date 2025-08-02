6d391da6c9f813e1676f42ae91859cb04dcc3e82
Mark Trevino <32989053+matrev@users.noreply.github.com>
2025-08-02 02:08:24 -0500

    switch from create-react-app to next.js

---
+++ next.config.tsx
@@ -0,0 +1,8 @@
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+    output: "export", // Enables static exports
+    reactStrictMode: true,
+}
+ 
+export default nextConfig
