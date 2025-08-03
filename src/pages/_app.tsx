947fa2d4147ad29365d901840b3bd909209dddb2
Mark Trevino <32989053+matrev@users.noreply.github.com>
2025-08-02 23:42:05 -0500

    removing unused and adding layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import Layout from 'components/layout'
 import type { AppProps } from 'next/app'
-import 'styles/layout.css'
+import Head from 'next/head'
+import 'styles/global.css'
  
 export default function App({ Component, pageProps }: AppProps) {
   return <Layout>
+    <Head>
+        <title>matrev</title>
+    </Head>
       <Component {...pageProps} />
   </Layout>
 }
