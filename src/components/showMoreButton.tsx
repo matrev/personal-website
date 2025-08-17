bb50c299e8ab4fd6bda8c7fe433fd31c56c6e37e
Mark Trevino <32989053+matrev@users.noreply.github.com>
2025-08-17 17:56:21 -0500

    show previous updates

---
+++ src/components/showMoreButton.tsx
@@ -0,0 +1,16 @@
+'use client'
+ 
+import { useState, ReactNode } from 'react'
+ 
+export default function ShowMoreButton({ children }: { children: ReactNode }) {
+  const [isMoreShown, updateMoreShown] = useState(false);
+
+  return (
+    <>
+        <button onClick={() => updateMoreShown(!isMoreShown)}>
+            {isMoreShown ? "Hide" : "Show More"}
+        </button>
+        {isMoreShown && children}
+    </>
+  )
+}
--- src/pages/now.tsx
+++ src/pages/now.tsx
@@ -1,6 +1,8 @@
 import path from 'path'
 import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 import { getMarkdownFiles, parseNowMarkdownFile, ParsedNowPost } from 'utils/markdownUtils';
+import { useState } from 'react';
+import ShowMoreButton from 'components/showMoreButton';
 
 export const getStaticProps = (async (context) => {
   const nowPostDirectory = path.join(process.cwd(), 'notes', 'WebsitePosts', 'NowPosts');
@@ -19,9 +21,11 @@ export const getStaticProps = (async (context) => {
 export default function Now({
   nowPostsContent,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [isMoreShown, setIsMoreShown] = useState(false);
+
   // Since the now post files are sorted in chronological order, the last post is the latest.
   const latestNowPost = nowPostsContent[nowPostsContent.length - 1];
-  // const remainingPosts = nowPostsContent.slice(0, nowPostsContent.length - 1);
+  const remainingPosts = nowPostsContent.slice(0, nowPostsContent.length - 1);
   return <>
         <h1>This is what I'm doing right about <a href="https://www.nownownow.com/about" target="_blank" rel='noopener noreferrer'>/now</a></h1>  
         <br />
@@ -29,13 +33,15 @@ export default function Now({
         <p>{latestNowPost?.data.date}</p>
         <p>{latestNowPost?.content}</p>
         <br/>
-    {/* {remainingPosts.reverse().map(post => (
-      <>
-        <h2>{post.data.title}</h2>
-        <p>{post.data.date}</p>
-        <p>{post.content}</p>
-        <br/>
-      </>
-    ))} */}
+    <ShowMoreButton>
+      {remainingPosts.reverse().map(post => (
+        <>
+          <h2>{post.data.title}</h2>
+          <p>{post.data.date}</p>
+          <p>{post.content}</p>
+          <br/>
+        </>
+      ))}
+    </ShowMoreButton>
   </>
 }
