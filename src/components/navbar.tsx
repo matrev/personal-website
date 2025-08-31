9263b4445b2f73fcdf32989a0bd6a00c3702f521
Mark Trevino <32989053+matrev@users.noreply.github.com>
2025-08-30 23:40:15 -0500

    adding hamburger functionality

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 import { useTheme } from './ThemeContext';
 
 export default function Navbar() {
@@ -12,20 +13,31 @@ export default function Navbar() {
     throw new Error('ThemeContext is undefined. Make sure NavBar is wrapped in ThemeProvider.');
   }
   const { theme, toggleTheme } = themeContext;
+  const [toggleMobileNav, setToggleMobileNav] = useState(false);
+
+  const toggleHamburgerButton = () => {
+    const nav = document.querySelector('.nav-items');
+    if (nav) {
+      nav.classList.toggle('open');
+    }
+  }
 
   return (
     <header>
-      <Link href="/" className={pathname === "/" ? 'selectedLink' : 'navLink'}>Home</Link>
-      <div className="pageNameNav">
-        <Link
-          href="/projects"
-          className={pathname.startsWith('/projects') ? 'selectedLink' : 'navLink'}
-        >Projects</Link>
-        <Link href="/now" className={pathname === "/now" ? 'selectedLink' : 'navLink'}>Now</Link>
-        <button className='themeToggleButton' onClick={toggleTheme}>
-          <Image src={`/${theme === 'dark' ? 'sun' : 'moon'}-svgrepo-com.png`} alt='test' width={24} height={24}/>
-        </button>
+      <div className="nav-items">
+        <Link href="/" className={pathname === "/" ? 'selectedLink' : 'navLink'}>Home</Link>
+          <Link
+            href="/projects"
+            className={pathname.startsWith('/projects') ? 'selectedLink' : 'navLink'}
+          >Projects</Link>
+          <Link href="/now" className={pathname === "/now" ? 'selectedLink' : 'navLink'}>Now</Link>
+          <button className='themeToggleButton' onClick={toggleTheme}>
+            <Image src={`/${theme === 'dark' ? 'sun' : 'moon'}-svgrepo-com.png`} alt='test' width={24} height={24}/>
+          </button>
       </div>
+      <button className="hamburger" onClick={toggleHamburgerButton}>
+        hamburger icon
+      </button>
     </header>
   );
 }
