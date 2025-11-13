894d2e939ada05a04ea85a95ef5ee3e2f665eb42
matrev <markanthonytrevinojr@gmail.com>
2025-11-13 07:18:53 -0600

    adding basic devportfolio

---
+++ src/pages/devPortfolio.tsx
@@ -0,0 +1,46 @@
+
+export default function DevPortfolio() {
+  return <>
+    <h1>Hello! I'm Mark.</h1>
+    <p>I am a full stack software engineer with over three years of experience building accessible, global, scalable applications.</p>
+
+    <h2>Work Experience</h2>
+    <ul>
+      <li>
+        <strong>Software Engineer at Microsoft</strong> (2022 - 2025)
+        <p>Full Stack web application development using C#, .NET, React, . Gained experience working with Power BI, Azure Data Factory, and Microsoft SQL Server for Business Intelligence. Worked on a resilient multi-cloud architecture with both Azure and AWS. Built IaC pipelines with AWS CloudFormation, PowerShell, and Bash scripts.</p>
+      </li>
+      <li>
+        <strong>Software Engineer Intern at Microsoft</strong> (Summer 2021)
+        <p>Contributed designing, coding, testing, and the deployment of various applications at Microsoft 365 in the 
+Release Notes and Service Health Communications space.</p>
+      </li>
+      <li>
+        <strong>Explorer Intern at Microsoft</strong> (Summer 2020)
+        <p>Worked with two other Explorer interns in designing, developing, testing, and deploying a web application that 
+validated an ML engine.</p>
+      </li>
+    </ul>
+
+    <h2>Skills</h2>
+    <ul>
+      <li>Languages: JavaScript, TypeScript, Python, Java, C++, C#, Bash, PowerShell</li>
+      <li>Frameworks: React, Node.js, Next.js</li>
+      <li>Databases: PostgreSQL, SQL</li>
+      <li>Tools: Git, Docker, AWS, Azure</li>
+    </ul>
+
+    {/* <h2>Projects</h2>
+    <ul>
+      <li>
+        <strong>Project A:</strong> A web application that allows users to track their tasks and manage projects. Built with React and Node.js.
+      </li>
+      <li>
+        <strong>Project B:</strong> An e-commerce platform that supports multiple payment gateways and provides a seamless shopping experience. Developed using Django and PostgreSQL.
+      </li>
+    </ul> */}
+
+    <h2>Education</h2>
+    <p>Bachelor of Science in Computer Science, Texas A&M University (2018 - 2022)</p>    
+  </>
+}
